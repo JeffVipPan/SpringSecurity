4a427720eabc828d4d349af10363d662060b2c02
jeffvippan <www.jeffpan@gmail.com>
2017-12-27 21:54:52 +0800

    add pom dependency

---
+++ security-demo/src/main/java/SpringSecurityDemoApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ек╥Е
+ * @date 2017/12/27 обнГ9:27
+ */
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+    public static void main(String[] args) {
+
+    }
+}
